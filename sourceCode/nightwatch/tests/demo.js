d77d69ee3ab507cee080629728699c87326d4bb8
Tom McKinney <tmckinney121@gmail.com>
2018-03-31 16:29:44 -0400

    Nightwatch can now be run using chromedriver + added a create-react-app typescript boilerplate to run tests against

--- sourceCode/nightwatch/tests/demo.js
+++ sourceCode/nightwatch/tests/demo.js
@@ -1,7 +1,9 @@
 module.exports = {
   'Demo test': (browser) => {
+    console.log(browser.launchUrl)
     browser
       .url(browser.launchUrl)
+      .waitForElementVisible('.App-logo', 5000)
       .end();
   }
 }
